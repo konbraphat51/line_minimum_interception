107c86a2109dd5cd8ef827f84601356efc18425f
konbraphat51 <konbraphat@gmail.com>
2023-12-01 08:27:47 +0900

    improve: show all components

--- unit_test.py
+++ unit_test.py
@@ -6,4 +6,7 @@ lines = []
 lines.append(Line(Vector(0, 0, 0), Vector(1, 0, 0)))
 lines.append(Line(Vector(1, 1, 1), Vector(0, 0, -1)))
 
-print(calculate_minimum_movements(lines))
+movement_vectors = calculate_minimum_movements(lines)
+
+for vector in movement_vectors:
+    print(vector)
