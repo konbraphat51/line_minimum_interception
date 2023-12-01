3e1d2ff9cdc08a204fceff1abc87b199c56d6d39
konbraphat51 <konbraphat@gmail.com>
2023-12-01 09:26:02 +0900

    test visualizer

---
+++ visualize.py
@@ -0,0 +1,18 @@
+#unit-test calculate_minimum_movements()
+
+from line_minimum_intersection import Vector, Line, calculate_minimum_movements, visualize_intersection
+
+lines = []
+lines.append(Line(Vector(0, 0, 0), Vector(1, 0, 0)))
+lines.append(Line(Vector(1, 1, 1), Vector(0, 0, -1)))
+
+movement_vectors = calculate_minimum_movements(lines)
+
+print("intersection point:")
+print(movement_vectors["intersection_point"])
+
+print("movement vectors:")
+for vector in movement_vectors["movement_vectors"]:
+    print(vector)
+    
+visualize_intersection(lines, movement_vectors["intersection_point"], movement_vectors["movement_vectors"])
