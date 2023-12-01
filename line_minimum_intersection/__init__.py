6d36688f9dc5b0f7b9a05afe1e136ffc9173f1d4
konbraphat51 <konbraphat@gmail.com>
2023-12-01 09:05:07 +0900

    Add: export

--- line_minimum_intersection/__init__.py
+++ line_minimum_intersection/__init__.py
@@ -1,8 +1,10 @@
 from .GeometryExpressions import Vector, Line
 from .MovementsOptimizer import calculate_minimum_movements
+from .Visualizer import visualize_intersection
 
 __all__ = [
     "Vector",
     "Line",
-    "calculate_minimum_movements"
+    "calculate_minimum_movements",
+    "visualize_intersection",
 ]
